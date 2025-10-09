3cc8c7e8a64eeae6c93e4c4c5d1326c6fc0075b2
kelvin-karanja26170 <kelvinkaranja8985@gmail.com>
2025-10-09 20:18:33 +0300

    Add files via upload

---
+++ printing_numbers.c
@@ -0,0 +1,15 @@
+/*
+Name:Kelvin Karanja
+Reg no:CT100/G/26170/25
+Description:Printing numbers
+*/
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	for(i=100; i>=50;i--){
+		printf("%d\n",i);
+	}
+	return 0;
+}
